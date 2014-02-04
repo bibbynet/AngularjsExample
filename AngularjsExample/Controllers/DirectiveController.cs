24ecb05d1f9b4ca16a85ef6c54dc0ec6d726912b
Bibby <bibbynet@gmail.com>
2014-02-04 17:37:33 +0800

    Init

---
+++ AngularjsExample/Controllers/DirectiveController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularjsExample.Controllers
+{
+    public class DirectiveController : Controller
+    {
+        //
+        // GET: /Directive/
+        public ActionResult Expander()
+        {
+            return View();
+        }
+	}
+}
